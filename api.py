3385312c4ded191cb0460f222aa292af22747735
struts2spring <struts2spring@gmail.com>
2017-12-14 15:00:36 +0530

    save

--- api.py
+++ api.py
@@ -4,11 +4,16 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from src.controllers.helloController import HelloController
+import os
  
 app = Flask(__name__)
  
 api = Api(app)
- 
+print("----------------------")
+print(os.environ)
+print("----------------------")
+# print(os.environ['DATABASE_URL'])
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///static/db/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
  
@@ -18,5 +23,7 @@ db = SQLAlchemy(app)
 api.add_resource(HelloController, '/api/hello')
 
 if __name__ == '__main__':
-    print(platform.python_version())
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000)) 
+#     print(platform.python_version())
+#     print(port)
+    app.run(host='0.0.0.0', port=port, debug=True)
