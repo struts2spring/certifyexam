426c942f309683992f9a556e6ce7e78d89af3981
struts2spring <struts2spring@gmail.com>
2017-12-14 12:51:23 +0530

    first commit

---
+++ src/controllers/helloController.py
@@ -0,0 +1,20 @@
+'''
+Created on 14-Dec-2017
+
+@author: vijay
+'''
+from flask_restful import Resource
+
+class HelloController(Resource):
+
+    def get(self):
+        return {"response" : "hello get"} 
+    
+    def post(self):
+        return {"response" : "hello post"}
+
+    def put(self):
+        return {"response" : "hello put"}
+
+    def delete(self):
+        return {"response" : "hello delete"}
---
+++ src/main.py
@@ -0,0 +1,14 @@
+'''
+Created on 13-Dec-2017
+
+@author: vijay
+'''
+
+import os
+
+
+
+
+
+if __name__=="__main__":
+    pass
