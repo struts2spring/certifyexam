d37a2f7494019f7f1b8340a722ec115ac822bc96
struts2spring <struts2spring@gmail.com>
2017-12-14 16:12:12 +0530

    save

--- api.py
+++ api.py
@@ -1,48 +1,29 @@
-import platform
  
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from src.controllers.helloController import HelloController
-import os
+from src.service.userService import UserService
  
 app = Flask(__name__)
  
 api = Api(app)
-print("----------------------")
-print(os.environ)
-print("----------------------")
+
 # print(os.environ['DATABASE_URL'])
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['HEROKU_POSTGRESQL_AQUA_URL']
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///static/db/test.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['HEROKU_POSTGRESQL_AQUA_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///static/db/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
  
  
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-    def __repr__(self):
-        return '<User %r>' % self.username
 
  
 api.add_resource(HelloController, '/api/hello')
 
 if __name__ == '__main__':
-    db.create_all()
-    user = User('John Doe', 'john.doe@example.com')
-    db.session.add(user)
-    all_users = User.query.all()
-    print(all_users)
-    db.session.commit()
+    UserService().initDatabase()
 #     print(platform.python_version())
 #     print(port)
     app.run(debug=True)
--- src/controllers/helloController.py
+++ src/controllers/helloController.py
@@ -4,16 +4,28 @@ Created on 14-Dec-2017
 @author: vijay
 '''
 from flask_restful import Resource
+# from src.service.userService import UserService
+# from src.model.user import User
+# from api import db
+
 
 class HelloController(Resource):
 
     def get(self):
+#         UserService().getUsers()
+#         db.create_all()
+
+#         all_users = User.query.all()
+#         print(all_users)
         return {"response" : "hello get"} 
     
     def post(self):
         return {"response" : "hello post"}
 
     def put(self):
+#         user = User('John Doe', 'john.doe@example.com')
+#         db.session.add(user)
+#         db.session.commit()
         return {"response" : "hello put"}
 
     def delete(self):
