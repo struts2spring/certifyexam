f79225df8ee3e606212cb8d2c9a59f6d9037ddaf
struts2spring <struts2spring@gmail.com>
2017-12-14 13:15:56 +0530

    save

--- api.py
+++ api.py
@@ -1,20 +1,22 @@
-
+import platform
+ 
 from flask import Flask
 from flask_restful import Api
-from controllers import helloController
 from flask_sqlalchemy import SQLAlchemy
-
+from src.controllers.helloController import HelloController
+ 
 app = Flask(__name__)
-
+ 
 api = Api(app)
-
+ 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///static/db/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-
+ 
+ 
 db = SQLAlchemy(app)
-
-api.add_resource(helloController.HelloController, '/api/hello')
+ 
+api.add_resource(HelloController, '/api/hello')
 
 if __name__ == '__main__':
-app.run(debug=True)
+    print(platform.python_version())
+    app.run(debug=True)
