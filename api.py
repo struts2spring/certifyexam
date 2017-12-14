2e2e528383d6887eca3607f75379b6a5715ceae7
struts2spring <struts2spring@gmail.com>
2017-12-14 16:17:34 +0530

    save

--- api.py
+++ api.py
@@ -4,14 +4,15 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from src.controllers.helloController import HelloController
 from src.service.userService import UserService
+import os
  
 app = Flask(__name__)
  
 api = Api(app)
 
 # print(os.environ['DATABASE_URL'])
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['HEROKU_POSTGRESQL_AQUA_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///static/db/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['HEROKU_POSTGRESQL_AQUA_URL']
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///static/db/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
  
  
